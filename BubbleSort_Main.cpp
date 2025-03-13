e81fd6e54424be021e7ad10f457438d4000e3f32
FahreziP <fahrezipurwadianitra@gmail.com>
2025-03-13 11:34:16 +0700

    fourth commit (penambahan display element array)

--- BubbleSort_Main.cpp
+++ BubbleSort_Main.cpp
@@ -40,6 +40,20 @@ void bubbleSortArray(){
     }while(pass <= n-1); //Step 5
 }
 
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1) {
+            cout << " --> ";
+        }
+    }
+}
+
 
 int main(){
 
