307582dedc8127d4fbc675904fa5d8a243f1a036
FahreziP <fahrezipurwadianitra@gmail.com>
2025-03-13 11:31:32 +0700

    second commit (penambahan void input banyaknya elemen pada array)

--- BubbleSort_Main.cpp
+++ BubbleSort_Main.cpp
@@ -5,6 +5,25 @@ using namespace std;
 int arr[20];    //deklarasi variable global array dengan panjang 20
 int n;          //deklarasi variable global "n" untuk menyimpan elemen pada array
 
+//prosedur input
+void input(){
+    int d;
+    while (true){
+        cout <<  "Masukkan banyaknya elemen pada array = ";
+        cin >> n;
+        if (n <= 20){
+            break;
+        }
+        else {
+            cout << "\nArray maksimal 20 elemen.\n"; 
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+}
+
 int main(){
-    
+
 }
