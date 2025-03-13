7be8c36d55904047da6842cb4fdb9d2b9c431bd8
FahreziP <fahrezipurwadianitra@gmail.com>
2025-03-13 11:29:20 +0700

    first commit

---
+++ BubbleSort_Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];    //deklarasi variable global array dengan panjang 20
+int n;          //deklarasi variable global "n" untuk menyimpan elemen pada array
+
+int main(){
+    
+}
