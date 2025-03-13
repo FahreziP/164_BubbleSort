d35c972b16f36d1c88ccad006974e45b42b95ffd
FahreziP <fahrezipurwadianitra@gmail.com>
2025-03-13 11:33:03 +0700

    third commit (Penambahan bubble short array)

--- BubbleSort_Main.cpp
+++ BubbleSort_Main.cpp
@@ -24,6 +24,23 @@ void input(){
     cout << "=====================" << endl;
 }
 
+void bubbleSortArray(){
+    int pass = 1; //Step 1
+    do {
+        for (int j=0;j<=n-1-pass;j++){ //Step 2 
+            if (arr[j] > arr[j+1]) { //Step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+            }
+        }
+        pass = pass + 1; //Step 4
+    }while(pass <= n-1); //Step 5
+}
+
+
 int main(){
 
 }
